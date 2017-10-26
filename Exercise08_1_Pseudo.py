6ed7a1ef67afdab67795e6fefb82eae22f6fda94
jsh <joshuahobgood@gmail.com>
2017-10-26 09:29:25 -0400

    update

--- Exercise08_1_Pseudo.py
+++ Exercise08_1_Pseudo.py
@@ -1,5 +1,13 @@
 #Exercise 8, Python question 1
 #10/13/17, MMD
+#import os and packages
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+#set working directory
+os.chdir('C:\\Users\\jsh\\OneDrive\\github\\BioComp\\Intro_Biocomp_ND_318_Tutorial8\\')
 
 #Open files to read and write
 vcffile = open("Cflorida.vcf","r")
@@ -23,3 +31,15 @@ for :#look at old code to see how you looped over a file
 #Close files
 
 
+#for loop over each column
+    #if first row contains ".A" or ".a"
+    #then replace with "Cf.Sfa." followed by the 3 digit sample ID
+    #for every other row in a column, only include info between the first and second ":"
+        #if the info between the two colons is "." replace with "NA"
+    
+    #if first row contains ".G" or ".g"
+    #then replace with "Cf.Gai." followed by the 3 digit sample ID
+    #for every other row in a column, only include info between the first and second ":"
+        #if the info between the two colons is "." replace with "NA"
+
+#write this all to "CfloridaCounts.txt"
