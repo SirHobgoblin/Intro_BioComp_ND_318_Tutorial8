137ff10aea9d112cb7e1e5187e931c865c39244d
jsh <joshuahobgood@gmail.com>
2017-10-27 09:50:45 -0400

    updates to a new file name, new code

--- Exercise08_1_Pseudo_2.py
+++ Exercise08_1_Pseudo_2.py
@@ -10,11 +10,15 @@
 
 #import os and packages
 import os
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import scipy
+import fileinput
+import string
+import sys
 import re
+#import pandas as pd
+#import numpy as np
+#import matplotlib.pyplot as plt
+#import scipy
+
 #set working directory
 os.chdir('C:\\Users\\jsh\\OneDrive\\github\\BioComp\\Intro_Biocomp_ND_318_Tutorial8\\')
 
@@ -22,45 +26,37 @@ os.chdir('C:\\Users\\jsh\\OneDrive\\github\\BioComp\\Intro_Biocomp_ND_318_Tutori
 
 #Open files to read and write
 vcffile = open("Cflorida.vcf","r")
-#line = line.strip() #stips \n
-#line = vcffile.readline().strip() #advances to the next line and strips
-#vcffile.close() #closes file, do we want this, yet?
-
-
-outfile = open("CfloridaCounts.txt","w")
-#outfile.write(string + "\n") #appends line to file, but need to define string?
-#outfile.close() #closes file
-
-#create empty dictionary
-dictionary = []
+outfile = open('CfloridaCounts.txt','w')
 for line in vcffile:
-    line = line.strip()
-    cols = line.strip()
-    if cols[0] in dictionary:
-        print("Duplicate: " + cols[1])
-        break
-    else:
-        dictionary[cols[0]] = cols[1]
+    outfile.write(re.sub(r"[Cc][Ff](07)?\.[Aa]2?","Cf.Sfa",line,flags=re.M))
+    outfile.write(line + '\n')
+    vcffile.close()
+    outfile.close()
+
+    
+    
+#outfile.write(re.sub(r"[Cc][Ff](07)\.[Gg][2Aa]([Ii])?",line,flags=re.M)) this is for FL
+
 
 
 #re.sub(regexString,replacement,searchString)
 #The regex string we should search for the TX samples is:
-"[Cc][Ff](07)?\.[Aa]2?" and replace with "Cf.Sfa"
+#"[Cc][Ff](07)?\.[Aa]2?" and replace with "Cf.Sfa"
 
 #The regex string we should search for the FL samples is:
-"[Cc][Ff](07)\.[Gg][2Aa]([Ii])?" and replace with "Cf.Gai"
+#"[Cc][Ff](07)\.[Gg][2Aa]([Ii])?" and replace with "Cf.Gai"
 #assign regex to variable name, or compile to variable name
 
 
 #loop over file
-for :#look at old code to see how you looped over a file
+#for :#look at old code to see how you looped over a file
     #strip end of line
-    if : #how can you tell if this is the header line?
+   # if : #how can you tell if this is the header line?
         #write unchanged header line to file
-    elif : #how can you tell if this is the line with the column headings?
+    #elif : #how can you tell if this is the line with the column headings?
         #standardize (replace) sample names with TX and FL regexes
         #write new version of line to file
-    else: #now you're in the data
+    #else: #now you're in the data
         #replace full SNP info with allele counts only
         #replace missing data with NA
         #write new version of line to new file
